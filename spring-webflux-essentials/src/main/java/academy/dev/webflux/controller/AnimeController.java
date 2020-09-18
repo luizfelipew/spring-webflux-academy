432d59fabaff2307632bb2378b942e0c21fb19d8
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2020-09-18 09:07:51 -0300

    R2DBC Spring Query DSL

--- spring-webflux-essentials/src/main/java/academy/dev/webflux/controller/AnimeController.java
+++ spring-webflux-essentials/src/main/java/academy/dev/webflux/controller/AnimeController.java
@@ -6,9 +6,11 @@ import academy.dev.webflux.service.AnimeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("animes")
@@ -21,5 +23,10 @@ public class AnimeController {
     @GetMapping
     public Flux<Anime> listAll() {
         return animeService.findAll();
+    }
+
+    @GetMapping(path = "{id}")
+    public Mono<Anime> findById(@PathVariable("id") int id) {
+        return animeService.findById(id);
     }
 }
--- spring-webflux-essentials/src/main/java/academy/dev/webflux/repository/AnimeRepository.java
+++ spring-webflux-essentials/src/main/java/academy/dev/webflux/repository/AnimeRepository.java
@@ -2,7 +2,8 @@ package academy.dev.webflux.repository;
 
 import academy.dev.webflux.domain.Anime;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface AnimeRepository extends ReactiveCrudRepository<Anime, Integer> {
-
+    Mono<Anime> findById(int id);
 }
